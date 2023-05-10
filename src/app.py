3105b599a8362684dd99af78215223bddc48fbf5
Susanta Gautam <xuxant02@gmail.com>
2023-05-11 02:22:54 +0545

    Fix cors

--- src/app.py
+++ src/app.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 from questionairs import Questionairs
 import prediction
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://dass-prediction.mixerml.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post('/DASPridict')
 def predict_das(questionairs: Questionairs):
@@ -11,3 +26,7 @@ def predict_das(questionairs: Questionairs):
     labels = predict.predict()
     labels['name'] = questionairs.name
     return labels
+
+@app.get('/')
+def main():
+    return {"message": "DAS Prediction API"}
