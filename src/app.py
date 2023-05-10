c2c76b1304055a7dcebd748236eff26564fcfdec
Susanta Gautam <xuxant02@gmail.com>
2023-05-11 02:27:02 +0545

    Fix typo

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin=origins,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
